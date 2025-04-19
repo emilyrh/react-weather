2a2a9075d9d2202d70083548ad7bb4e5075d200c
emilyrh <114513651+emilyrh@users.noreply.github.com>
2025-04-19 16:37:33 +0100

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -3,25 +3,27 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This app was coded by{" "}
-        <a
-          href="https://www.shecodes.io/graduates/147891-emily-huggins"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Emily Huggins
-        </a>{" "}
-        and is{" "}
-        <a
-          href="https://github.com/emilyrh/react-weather"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          open-sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This app was coded by{" "}
+          <a
+            href="https://www.shecodes.io/graduates/147891-emily-huggins"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Emily Huggins
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/emilyrh/react-weather"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
