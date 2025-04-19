428f1751c729ccbea467f6f8f0111d2b66441ce2
emilyrh <114513651+emilyrh@users.noreply.github.com>
2025-04-19 16:04:37 +0100

    Added footer with links to SheCodes profile and GitHub repo

--- src/App.js
+++ src/App.js
@@ -4,6 +4,24 @@ export default function App() {
   return (
     <div className="App">
       <h1>Weather App</h1>
+      <footer>
+        This app was coded by{" "}
+        <a
+          href="https://www.shecodes.io/graduates/147891-emily-huggins"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Emily Huggins
+        </a>{" "}
+        and is{" "}
+        <a
+          href="https://github.com/emilyrh/react-weather"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          open-sourced on GitHub
+        </a>
+      </footer>
     </div>
   );
 }
