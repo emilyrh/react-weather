ca416dad9af075fd073de8de2adc76fcf508c56a
emilyrh <114513651+emilyrh@users.noreply.github.com>
2025-04-21 16:54:39 +0100

    Integrated API to show the forecast for the first day

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,32 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import WeatherIcon from "./WeatherIcon";
+import WeatherForecastDay from "./WeatherForecastDay";
 import "./WeatherForecast.css";
 import axios from "axios";
 
 export default function WeatherForecast(props) {
+  let [loaded, setLoaded] = useState(false);
+  let [forecast, setForecast] = useState(null);
+
   function handleResponse(response) {
-    console.log(response.data);
+    setForecast(response.data.daily);
+    setLoaded(true);
   }
   console.log(props);
-  let apiKey = "a12670296b5b7ac064111tf40b315o03";
-  let longitude = props.coordinates.longitude;
-  let latitude = props.coordinates.latitude;
-  let apiURL = `https://api.shecodes.io/weather/v1/forecast?lon=${longitude}&lat=${latitude}&key=${apiKey}&units=metric`;
 
-  axios.get(apiURL).then(handleResponse);
-
-  return (
-    <div className="WeatherForecast">
-      <div className="row">
-        <div className="col">
-          <div className="WeatherForecastDay">Tue</div>
-          <WeatherIcon code="clear-sky-day" size={36} />
-          <div className="WeatherForecast-temperature">
-            <span className="WeatherForcast-temperature-max">15º</span>
-            <span className="WeatherForecast-temperature-min">5º</span>
+  if (loaded) {
+    console.log(forecast);
+    return (
+      <div className="WeatherForecast">
+        <div className="row">
+          <div className="col">
+            <WeatherForecastDay data={forecast[0]} />
           </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    let apiKey = "a12670296b5b7ac064111tf40b315o03";
+    let longitude = props.coordinates.longitude;
+    let latitude = props.coordinates.latitude;
+    let apiURL = `https://api.shecodes.io/weather/v1/forecast?lon=${longitude}&lat=${latitude}&key=${apiKey}&units=metric`;
+
+    axios.get(apiURL).then(handleResponse);
+
+    return null;
+  }
 }
